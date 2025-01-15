344d73090336686813243748cde2b69cae9f43c8
nan <ipfilus@gmail.com>
2025-01-15 16:55:22 -0300

    feat: add create_order route

--- src/main/routes/orders_routes.py
+++ src/main/routes/orders_routes.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from src.main.http_types.http_request import HttpRequest
-from src.main.http_types.http_response import HttpResponse
+from src.main.composer.create_order_composer import create_order_composer
 
 orders_routes_bp = Blueprint("orders_routes", __name__)
 
 @orders_routes_bp.route("/orders", methods=["POST"])
 def create_order():
     http_request = HttpRequest(body=request.json)
-    http_response = HttpResponse(status_code=201, body={"message": "Order created successfully"})
-    return http_response
+    use_case = create_order_composer()
+    response = use_case.execute(http_request)
+    return jsonify(response.body), response.status_code
