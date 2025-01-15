f5d9457a30a9e9ab2aa86121b3a2b0854703c794
nan <ipfilus@gmail.com>
2025-01-15 14:09:58 -0300

    feat: add http_types

--- src/main/routes/orders_routes.py
+++ src/main/routes/orders_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
+from src.main.http_types.http_request import HttpRequest
+from src.main.http_types.http_response import HttpResponse
 
 orders_routes_bp = Blueprint("orders_routes", __name__)
 
 @orders_routes_bp.route("/orders", methods=["POST"])
 def create_order():
-    return jsonify({"message": "Order created successfully"}), 201
+    http_request = HttpRequest(body=request.json)
+    http_response = HttpResponse(status_code=201, body={"message": "Order created successfully"})
+    return http_response
